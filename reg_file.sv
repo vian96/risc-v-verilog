b03f478ce6f48c76acfccf36cbf9251c48b3ecba
user.name <vianmart@yandex.ru>
2025-05-26 19:22:20 +0300

    fixxed regfile

--- reg_file.sv
+++ reg_file.sv
@@ -15,8 +15,8 @@ module reg_file (
   logic [31:0] registers[32];
 
   // TODO: KOSTYL?
-  assign d1 = (a1 != 0) ? ((a1 != a3) ? registers[a1] : wd) : 32'b0;
-  assign d2 = (a2 != 0) ? ((a2 != a3) ? registers[a2] : wd) : 32'b0;
+  assign d1 = (a1 != 0) ? registers[a1] : 32'b0;
+  assign d2 = (a2 != 0) ? registers[a2] : 32'b0;
 
   always_ff @(posedge dump) begin
     $display("--- Displaying regs at %0t ---", $time);
@@ -27,6 +27,12 @@ module reg_file (
   end
 
   always_ff @(posedge clk) begin
+    $display(
+        "Time %0t: \033[35m Reg file \033[0m -> Address = %d, val = 0x%h, enable = %d, %d = 0x%h, %d = 0x%h",
+        $time, a3, wd, we3, a1, d1, a2, d2);
+  end
+
+  always_ff @(negedge clk) begin
     $display(
         "Time %0t: \033[35m Reg file \033[0m -> Address = %d, val = 0x%h, enable = %d, %d = 0x%h, %d = 0x%h",
         $time, a3, wd, we3, a1, d1, a2, d2);
